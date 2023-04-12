cdf6ce098e673e2d1deed1e65ebbdfa45f09fc45
Rookin_work <crazynick21@gmail.com>
2023-04-12 15:41:37 +0300

    Начало описания функционала кнопок

--- EducationDudar/MainWindow.xaml.cs
+++ EducationDudar/MainWindow.xaml.cs
@@ -1,28 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EducationDudar
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach(UIElement el in MainRoot.Children) // Обработка событий
+            {
+                if(el is Button)
+                {
+                    ((Button) el).Click += ButtonClick;
+                }
+            }
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
