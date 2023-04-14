d58dc56a7ecc76f5a1a3186f05076379dbfb09f1
Rookin_work <crazynick21@gmail.com>
2023-04-14 08:39:00 +0300

    Базоый рабочий калькулятор

--- EducationDudar/MainWindow.xaml.cs
+++ EducationDudar/MainWindow.xaml.cs
@@ -18,19 +18,24 @@ namespace EducationDudar
                 }
             }
         }
-
+    
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
             string str = (string)((Button)e.OriginalSource).Content; // Обращение к строке. (Button) и (string) - присвоение типа 
 
             if (str == "AC")
+            {
                 TextLabel.Text = "";
+            }               
             else if (str == "=")
             {
                 string value = new DataTable().Compute(TextLabel.Text, null).ToString();
                 TextLabel.Text = value;
             }
+            else
+            {
                 TextLabel.Text += str;
+            }
         }
     }
 }
